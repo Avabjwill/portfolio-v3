04e60cf5b4c771d3130b5ed804df0e493b7e2a1d
Avabjwill <81047652+Avabjwill@users.noreply.github.com>
2021-10-12 22:57:36 -0400

    re-focused submission

--- Main/src/components/Resume/resume.js
+++ Main/src/components/Resume/resume.js
@@ -15,19 +15,15 @@ function Resume() {
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>responsive design</li>
           <li>React</li>
-          <li>Bootstrap</li>
         </ul>
-        <h3>Back-end Proficiencies</h3>
+        <h3>Back-end Proficiencies/Capabilities</h3>
         <ul className="skills">
-          <li>APIs</li>
           <li>Node</li>
+          <li>MeanStack</li>
           <li>Express</li>
           <li>MySQL, Sequelize</li>
           <li>MongoDB, Mongoose</li>
-          <li>REST</li>
           <li>GraphQL</li>
         </ul>
       </div>
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # portfolio-v3
+
+React Portfolio
+
+This is a react based Portfolio. 
+
+This Portfolio contains Projects which have been created.
